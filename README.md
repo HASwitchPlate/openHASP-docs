9b5285bdec51dbbac6b9455935407974a2da3bca
fvanroie <cpt_jack@msn.com>
2021-07-05 00:59:49 +0200

    Update dev site

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# openHASP 0.6 documentation
+# openHASP 0.6.1 documentation
 
-[Click here](https://haswitchplate.github.io/openHASP-docs/0.6/) to access the manual in a human readable form.
+[Click here](https://haswitchplate.github.io/openHASP-docs/0.6.1/) to access the manual in a human readable form.
--- custom_theme/js/youtube.js
+++ custom_theme/js/youtube.js
@@ -7,14 +7,16 @@
 //     });
 // });
 
+// Load Videos in the Docs
 $(document).ready(function() {
     $('img[alt="YOUTUBE"]').each(function() {
         var id = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
-        var video = '<iframe title = "YouTube video player" width="560" height="315" src="https://www.youtube.com/embed/' + id + '?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        var video = '<iframe title="YouTube video player" width="560" height="320" src="https://www.youtube.com/embed/' + id + '?controls=1" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         $(this).replaceWith(video);
     });
 });
 
+// Open external links in new window
 $(document.links).filter(function() {
     return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
