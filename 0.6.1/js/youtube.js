c220b1893f56d0de9e28951e057e4f380b489514
fvanroie <fvanroie@users.noreply.github.com>
2021-07-07 02:32:05 +0000

    Deployed 504a0be9 to 0.6.1 with MkDocs 1.2.1 and mike 1.0.1

--- 0.6.1/js/youtube.js
+++ 0.6.1/js/youtube.js
@@ -8,15 +8,10 @@
 // });
 
 // Load Videos in the Docs
-$(document).ready(function() {
-    $('img[alt="YOUTUBE"]').each(function() {
-        var id = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
-        var video = '<div class="video-container"><iframe title="YouTube video player" src="https://www.youtube.com/embed/' + id + '?rel=0controls=1" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
-        $(this).replaceWith(video);
-    });
-});
-
-// Open external links in new window
-$(document.links).filter(function() {
-    return this.hostname != window.location.hostname;
-}).attr('target', '_blank');
+// $(document).ready(function() {
+//     $('img[alt="YOUTUBE"]').each(function() {
+//         var id = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
+//         var video = '<div class="embed-responsive embed-responsive-16by9" style="max-width:560px; margin:auto;"><iframe title="YouTube video player" src="https://www.youtube.com/embed/' + id + '?rel=0&controls=1" class="embed-responsive-item" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+//         $(this).replaceWith(video);
+//     });
+// });
